ab1ad23fbcfe3180966476706cb4bacd0560f226
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-09-25 18:58:42 +0200

    feature: add fingeprint method to spotify partial playlist

---
+++ aqoo/SPFPartialPlaylistExt.swift
@@ -0,0 +1,23 @@
+//
+//  SPFPartialPlaylistExt.swift
+//  aqoo
+//
+//  Created by Patrick Paechnatz on 25.09.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+import Spotify
+
+extension SPTPartialPlaylist {
+
+    func getMD5FingerPrint() -> String {
+        
+        return String(
+            format: "%@:%@:%@:%@",
+            self.name,
+            self.trackCount,
+            "\(self.isPublic)",
+            "\(self.isCollaborative)").md5()
+    }
+}
