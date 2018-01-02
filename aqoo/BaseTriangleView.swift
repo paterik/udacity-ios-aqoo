cb2d84dab4416ac505cb4eccd7b91ea089e40ea2
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2018-01-02 10:03:30 +0100

    add: new base triangle view component

---
+++ aqoo/BaseTriangleView.swift
@@ -0,0 +1,35 @@
+//
+//  BaseTriangleView.swift
+//  aqoo
+//
+//  Created by Patrick Paechnatz on 31.12.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseTriangleView : UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        context.beginPath()
+        context.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        context.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        context.addLine(to: CGPoint(x: (rect.maxX / 2.0), y: rect.minY))
+        context.closePath()
+        
+        context.setFillColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 0.60)
+        context.fillPath()
+    }
+}
