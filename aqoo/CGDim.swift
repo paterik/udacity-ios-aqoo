0aaf9e8860181e0bfcca763f25bb2cf987503eb8
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2018-01-04 12:44:31 +0100

    fix: add refresh table extension

--- aqoo/CGDim.swift
+++ aqoo/CGDim.swift
@@ -9,19 +9,30 @@
 import Foundation
 import UIKit
 
-extension CGRect{
+extension CGRect {
+    
     init(_ x:CGFloat,_ y:CGFloat,_ width:CGFloat,_ height:CGFloat) {
         self.init(x:x,y:y,width:width,height:height)
     }
     
 }
-extension CGSize{
+extension CGSize {
+    
     init(_ width:CGFloat,_ height:CGFloat) {
         self.init(width:width,height:height)
     }
 }
-extension CGPoint{
+extension CGPoint {
+    
     init(_ x:CGFloat,_ y:CGFloat) {
         self.init(x:x,y:y)
+    }
+}
+
+extension UITableView {
+    
+    func refreshTable() {
+        let indexPathForSection = NSIndexSet(index: 0)
+        self.reloadSections(indexPathForSection as IndexSet, with: UITableViewRowAnimation.middle)
     }
 }
