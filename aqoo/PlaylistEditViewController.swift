5eb65e6580e260cbedef124d11d21b915c8700bf
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2018-01-05 11:20:56 +0100

    feature: add new playlistEditView segue processor including controller and view config

---
+++ aqoo/PlaylistEditViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PlaylistEditViewController.swift
+//  aqoo
+//
+//  Created by Patrick Paechnatz on 05.01.18.
+//  Copyright © 2018 Patrick Paechnatz. All rights reserved.
+//
+
+
+import UIKit
+import Spotify
+import Kingfisher
+import FoldingCell
+import BGTableViewRowActionWithImage
+
+class PlaylistEditViewController:   BaseViewController,
+                                    SPTAudioStreamingPlaybackDelegate,
+                                    SPTAudioStreamingDelegate {
+
+    var playlistInDb: StreamPlayList?
+    var playListInCloud: SPTPartialPlaylist?
+    
+    @IBOutlet weak var navItemEditViewTitle: UINavigationItem!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        UIApplication.shared.statusBarStyle = UIStatusBarStyle.lightContent
+        
+        navItemEditViewTitle.title = playlistInDb!.name
+        
+        print ("LOAD_PLAYLIST -> [cache: \(playlistInDb!.name)]")
+    }
+    
+    @IBAction func btnExitEditViewAction(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ aqoo/PlaylistEditViewControllerExt.swift
@@ -0,0 +1,13 @@
+//
+//  PlaylistEditViewControllerExt.swift
+//  aqoo
+//
+//  Created by Patrick Paechnatz on 05.01.18.
+//  Copyright © 2018 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+
+extension PlaylistEditViewController {
+    
+}
