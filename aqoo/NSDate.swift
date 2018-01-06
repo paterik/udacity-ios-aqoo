cfa36ee5e1303feaa13f235122d27ae163b77d96
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2018-01-06 11:03:28 +0100

    feature: add new extension for NSDate to print out string formatted date values

---
+++ aqoo/NSDate.swift
@@ -0,0 +1,35 @@
+//
+//  NSDate.swift
+//  aqoo
+//
+//  Created by Patrick Paechnatz on 06.01.18.
+//  Copyright © 2018 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+
+extension NSDate {
+    
+    func dateToString(
+        _ date: Date!,
+        _ format: String) -> NSString {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        
+        return dateFormatter.string(from: date) as NSString
+    }
+    
+    func dateFromString(
+        _ date: NSString,
+        _ format: String) -> NSDate {
+        
+        let formatter = DateFormatter()
+        let locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        
+        formatter.locale = locale as Locale!
+        formatter.dateFormat = format
+        
+        return formatter.date(from: date as String)! as NSDate
+    }
+}
