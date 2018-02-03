9f2ad19fc1c30659562ca16ecf9f26b3353e4792
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2018-02-03 15:33:04 +0100

    feature: extend string class by random generator

--- aqoo/String.swift
+++ aqoo/String.swift
@@ -17,4 +17,17 @@ extension String {
     var dropLast: String {
         return dropLast()
     }
+    
+    static func random(length: Int = 20) -> String {
+        
+        let base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        var randomString: String = ""
+        
+        for _ in 0..<length {
+            let randomValue = arc4random_uniform(UInt32(base.count))
+                randomString += "\(base[base.index(base.startIndex, offsetBy: Int(randomValue))])"
+        }
+        
+        return randomString
+    }
 }
