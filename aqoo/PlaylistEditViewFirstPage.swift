0957a1a80f42f6d9852ed1b198e8b2ef575c9044
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2018-06-13 22:18:11 +0200

    feature: add base cover to playlist editView / firstpage

--- aqoo/PlaylistEditViewFirstPage.swift
+++ aqoo/PlaylistEditViewFirstPage.swift
@@ -13,4 +13,65 @@ import Kingfisher
 
 class PlaylistEditViewFirstPage: BasePlaylistEditViewController {
     
+    //
+    // MARK: Class LowLevel Variables
+    //
+    
+    var _noCoverImageAvailable: Bool = true
+    var _noCoverOverrideImageAvailable: Bool = true
+    
+    @IBOutlet weak var imgPlaylistCoverBig: UIImageView!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        setupUICoverImages()
+    }
+    
+    //
+    // MARK: Class Method Overloads
+    //
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        UIApplication.shared.statusBarStyle = UIStatusBarStyle.lightContent
+    }
+
+    func setupUICoverImages() {
+        
+        var _playListCoverURL: String?
+        
+        // evaluate the "perfect" cover for our detailView
+        if (playListInDb!.largestImageURL != nil) {
+            _playListCoverURL = playListInDb!.largestImageURL!
+            _noCoverImageAvailable = false
+        }   else if (playListInDb!.smallestImageURL != nil) {
+            _playListCoverURL = playListInDb!.smallestImageURL!
+            _noCoverImageAvailable = false
+        }
+        
+        // cover image url available - using kf processing technics and render one
+        if _noCoverImageAvailable == false {
+            imgPlaylistCoverBig.kf.setImage(
+                with: URL(string: _playListCoverURL!),
+                placeholder: UIImage(named: _sysDefaultCoverImage),
+                options: [
+                    .transition(.fade(0.2)),
+                    .processor(ResizingImageProcessor(referenceSize: _sysPlaylistCoverDetailImageSize))
+                ]
+            )
+        }
+        
+        if  playListInDb!.coverImagePathOverride != nil {
+            _noCoverOverrideImageAvailable = false
+            
+            if let _image = getImageByFileName(playListInDb!.coverImagePathOverride!) {
+                imgPlaylistCoverBig.alpha = _sysPlaylistCoverOriginInActiveAlpha
+            }   else {
+                _handleErrorAsDialogMessage("IO Error (Read)", "unable to load your own persisted image for your playlist")
+            }
+        }
+    }
 }
