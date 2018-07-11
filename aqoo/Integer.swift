f896cfabb0df2213663a71d689479b8c1a6cc925
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2018-07-11 13:30:05 +0200

    feature: add integer extension to convert bigger values into human readable formats

---
+++ aqoo/Integer.swift
@@ -0,0 +1,67 @@
+//
+//  Integer.swift
+//  aqoo
+//
+//  Created by Patrick Paechnatz on 11.07.18.
+//  Copyright © 2018 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+
+extension Int32 {
+    // max: 2.147.483.647
+    var hrFormatted: String {
+        
+        if  self >= 1000, self <= 999999 {
+            return String(format: "%.1dk", locale: Locale.current, self / 1000)
+        }
+        
+        if  self > 999999 {
+            return String(format: "%.1dM", locale: Locale.current, self / 1000000)
+        }
+        
+        // stop handling numbers beyond type maximum ;)
+        if  self >= 2147483647 {
+            return "∞"
+        }
+        
+        if  self == 0 {
+            return "-"
+        }
+        
+        return String(format: "%.0d", locale: Locale.current, self)
+    }
+}
+
+extension Int64 {
+    // max: 9.223.372.036.854.775.807
+    var hrFormatted: String {
+        
+        if  self >= 1000, self <= 999999 {
+            return String(format: "%.1dk", locale: Locale.current, self / 1000)
+        }
+        
+        if  self > 999999 {
+            return String(format: "%.1dM", locale: Locale.current, self / 1000000)
+        }
+        
+        if  self > 999999999 {                                             999999999
+            return String(format: "%.1dB", locale: Locale.current, self / 1000000000)
+        }
+        
+        if  self > 999999999999 {
+            return String(format: "%.1dT", locale: Locale.current, self / 1000000000000)
+        }
+        
+        // stop handling numbers beyond quatrillion ;)
+        if  self > 999999999999999 {
+            return "∞"
+        }
+        
+        if  self == 0 {
+            return "-"
+        }
+        
+        return String(format: "%.0d", locale: Locale.current, self)
+    }
+}
