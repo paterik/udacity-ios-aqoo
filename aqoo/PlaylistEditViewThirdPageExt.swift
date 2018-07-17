a9a7c03807e192c2333fd473f6e66cfafb1d4133
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2018-07-17 13:34:51 +0200

    feature: add new extensions for editViewTabPageControllers (1-3)

---
+++ aqoo/PlaylistEditViewThirdPageExt.swift
@@ -0,0 +1,79 @@
+//
+//  PlaylistEditViewThirdPageExt.swift
+//  aqoo
+//
+//  Created by Patrick Paechnatz on 17.07.18.
+//  Copyright © 2018 Patrick Paechnatz. All rights reserved.
+//
+
+import Spotify
+import CoreStore
+import Kingfisher
+
+extension PlaylistEditViewThirdPage {
+    
+    //
+    // MARK: Class Setup UI/UX Functions
+    //
+    
+    func setupUIBase() {
+        
+        // playlist details will always be updateable
+        btnSavePlaylistChanges.isEnabled = true
+    }
+    
+    func setupUIPlaylistDetails() {
+        
+        textFieldPlaylistDetails.text = playListInDb!.metaListInternalDescription
+    }
+    
+    func handlePlaylistMetaUpdate() {
+        
+        var _playlistDetails: String = textFieldPlaylistDetails.text
+        
+        CoreStore.perform(
+            asynchronous: { (transaction) -> Void in
+                
+                // find persisted playlist object from local cache (db)
+                guard let playlistToUpdate = transaction.fetchOne(
+                    From<StreamPlayList>().where(\.metaListHash == self.playListInDb!.metaListHash))
+                    as? StreamPlayList else {
+                        self.handleErrorAsDialogMessage(
+                            "Cache Error", "unable to fetch playlist from local cache"
+                        );   return
+                }
+                
+                playlistToUpdate.updatedAt = Date()
+                playlistToUpdate.metaNumberOfUpdates += 1
+                playlistToUpdate.metaPreviouslyUpdatedManually = true
+                playlistToUpdate.metaListInternalDescription = _playlistDetails
+                
+                self.playListInDb = playlistToUpdate
+            },
+            completion: { (result) -> Void in
+                switch result {
+                    
+                case .failure(let error):
+                    
+                    self.handleBtnSavePlaylistChangesState( active: false )
+                    self.handleErrorAsDialogMessage(
+                        "Cache Error", "unable to update playlist local cache"
+                    )
+                    
+                case .success(let userInfo):
+                    
+                    // delegate information about current playlist state to parentView
+                    self.handleBtnSavePlaylistChangesState( active: true )
+                    if  let delegate = self.delegate {
+                        delegate.onPlaylistChanged( self.playListInDb! )
+                    }
+                }
+            }
+        )
+    }
+    
+    func handleBtnSavePlaylistChangesState(active: Bool) {
+        
+        btnSavePlaylistChanges.isEnabled = active
+    }
+}
