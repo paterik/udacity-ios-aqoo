aaf0c6abcd9e1e26c1f9bbfbeb0b21f5e00bae29
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2018-07-19 18:31:59 +0200

    feature: prepare new playlist extended meta handling (incl tracks)

---
+++ aqoo/StreamPlayListExtended.swift
@@ -0,0 +1,16 @@
+//
+//  StreamPlayListAdvanced.swift
+//  aqoo
+//
+//  Created by Patrick Paechnatz on 19.07.18.
+//  Copyright © 2018 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+
+class StreamPlayListAdvanced {
+    
+    var playlistIdentifier: String?
+    var playlistSnapshotId: String?
+    var playlistFollowerCount: Int?
+}
